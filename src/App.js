e8240cf213158fc5204063e8cc48a8c6e4b5ab00
KaitlynDodds <kadodds.dev@kadodds.com>
2018-09-28 12:08:51 -0600

    added searchform component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+// components
+import SearchForm from './components/SearchForm';
+
 class App extends Component {
   render() {
     return (
       <div className="container">
-        
+        <SearchForm />
       </div>
     );
   }
