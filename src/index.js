7c2b3660107af78623f6f8e8f5374371c9e1cfcc
KaitlynDodds <kadodds.dev@kadodds.com>
2018-10-03 12:13:35 -0600

    removing call to dotenv dependency

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-require('dotenv').config();
+// require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
